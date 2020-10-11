af4b7c9efb6911484640018f853cc1a8e9228e6d
kevinoliveros-dev <kevin.oliveros0108@gmail.com>
2020-10-11 21:17:06 +0800

    OmuMvc update

--- AdminLteMvc-master/AdminLteMvc/AdminLteMvc/Controllers/EIRController.cs
+++ AdminLteMvc-master/AdminLteMvc/AdminLteMvc/Controllers/EIRController.cs
@@ -10,6 +10,7 @@ using System.Web.Mvc;
 using AdminLteMvc.Models;
 using AdminLteMvc.Models.WEBSales;
 using CrystalDecisions.CrystalReports.Engine;
+using Omu.AwesomeMvc;
 
 namespace AdminLteMvc.Controllers
 {
