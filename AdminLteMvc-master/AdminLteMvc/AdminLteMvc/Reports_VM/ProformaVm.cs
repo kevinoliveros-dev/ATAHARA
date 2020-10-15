fabd00cb6e6832905ee535b5c3cb65694bae5d08
kevinoliveros-dev <kevin.oliveros0108@gmail.com>
2020-10-16 03:51:32 +0800

    Update Reports,script and MVC Controller

---
+++ AdminLteMvc-master/AdminLteMvc/AdminLteMvc/Reports_VM/ProformaVm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminLteMvc.Reports_VM
+{
+    public class ProformaVm
+    {
+        public int proformaBillID { get; set; }
+        public string proformaBillRefNo { get; set; }
+        public string proformaBillDate { get; set; }
+        public string proformaBillVesselName { get; set; }
+        public string proformaBillVoyageNo { get; set; }
+        public string proformaBillDestination { get; set; }
+        public string proformaBillShipper { get; set; }
+        public string proformaBillShippersAddress { get; set; }
+        public string proformaBillShippersTelNo { get; set; }
+        public string proformaBillServiceType { get; set; }
+        public string proformaBillConsignee { get; set; }
+        public string proformaBillConsigneesAddress { get; set; }
+        public string proformaBillConsigneesTelNo { get; set; }
+        public string proformaBillNo { get; set; }
+        public string proformaBillMarks { get; set; }
+        public string proformaBillQty { get; set; }
+        public string proformaBillUnit { get; set; }
+        public string proformaBillDescriptionOfCargo { get; set; }
+        public string proformaBillValue { get; set; }
+        public string proformaBillWeight { get; set; }
+        public string proformaBillMeasurement { get; set; }
+        public string proformaBillRemarks { get; set; }
+        public string proformaBillMeasuredBy { get; set; }
+        public string proformaBillTruckersName { get; set; }
+        public string proformaBillShippersName { get; set; }
+    }
+}
